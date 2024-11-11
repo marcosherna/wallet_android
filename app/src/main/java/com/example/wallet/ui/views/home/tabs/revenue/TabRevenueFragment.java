63531170b23c725ffabff671a138808d53e8b761
marcosherna <marcosxdf6@gmail.com>
2024-11-10 21:44:13 -0600

    version preliminar del tab ingresos

--- app/src/main/java/com/example/wallet/ui/views/home/tabs/revenue/TabRevenueFragment.java
+++ app/src/main/java/com/example/wallet/ui/views/home/tabs/revenue/TabRevenueFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.wallet.databinding.FragmentTabRevenueBinding;
 import com.example.wallet.domain.fake.repository.AccountMovementRepository;
@@ -19,27 +20,52 @@ import com.example.wallet.ui.adapters.RVAccountMovementAdapter;
 
 import java.util.ArrayList;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class TabRevenueFragment extends Fragment {
     FragmentTabRevenueBinding binding;
+    TabRevenueViewModel tabRevenueViewModel;
+    final CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        TabRevenueViewModel tabRevenueViewModel = new ViewModelProvider(this).get(TabRevenueViewModel.class);
+        tabRevenueViewModel = new ViewModelProvider(this).get(TabRevenueViewModel.class);
         binding = FragmentTabRevenueBinding.inflate(inflater, container, false);
+        RVAccountMovementAdapter accountMovementAdapter = new RVAccountMovementAdapter();
+        binding.rvAllAccountRevenue.setAdapter(accountMovementAdapter);
+        tabRevenueViewModel.getMovements().observe(getViewLifecycleOwner(), accountMovementAdapter::setItems);
 
-        AccountMovementRepository repository = new AccountMovementRepository();
-        ArrayList<AccountMovement> lst = repository.getAllByType(AccountMovement.Type.REVENUE);
-        binding.rvAllAccountRevenue.setAdapter(new RVAccountMovementAdapter());
+        this.binding.swpRefreshLayout.setOnRefreshListener(this::loadData);
+        this.loadData();
 
         return binding.getRoot();
+    }
+
+    private void loadData(){
+        this.binding.swpRefreshLayout.setRefreshing(true);
+        this.compositeDisposable.add(
+                tabRevenueViewModel.initialize()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                () -> this.binding.swpRefreshLayout.setRefreshing(false),
+                                throwable ->  {
+                                    this.binding.swpRefreshLayout.setRefreshing(false);
+                                    Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                                }
+                        )
+        );
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        compositeDisposable.clear();
     }
 
 }
--- app/src/main/java/com/example/wallet/ui/views/home/tabs/revenue/TabRevenueViewModel.java
+++ app/src/main/java/com/example/wallet/ui/views/home/tabs/revenue/TabRevenueViewModel.java
@@ -1,7 +1,47 @@
 package com.example.wallet.ui.views.home.tabs.revenue;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.wallet.domain.fake.repository.AccountMovementRepository;
+import com.example.wallet.domain.models.AccountMovement;
+import com.example.wallet.ui.mappers.Mapper;
+import com.example.wallet.ui.models.AccountMovementUI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import io.reactivex.rxjava3.core.Completable;
+
 public class TabRevenueViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    final AccountMovementRepository accountMovementRepository;
+    final MutableLiveData<List<AccountMovementUI>> movements;
+    public LiveData<List<AccountMovementUI>> getMovements(){ return this.movements; }
+    public TabRevenueViewModel(){
+        this.accountMovementRepository = new AccountMovementRepository();
+        this.movements = new MutableLiveData<>(new ArrayList<>());
+    }
+
+    public Completable initialize(){
+        return Completable.create(emitter -> {
+            try {
+                Thread.sleep(1000);
+                List<AccountMovementUI> movementUIS = this.getMovementsRevenue();
+                this.movements.postValue(movementUIS);
+                emitter.onComplete();
+            }catch (Exception e){
+                emitter.onError(e);
+            }
+        });
+    }
+
+    private List<AccountMovementUI> getMovementsRevenue(){
+        return  this.accountMovementRepository.getAllByType(AccountMovement.Type.REVENUE).stream()
+                .map(Mapper::AMovementToUI)
+                .collect(Collectors.toList());
+
+    }
+
 }
